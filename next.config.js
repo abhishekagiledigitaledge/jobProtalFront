9980c767dbff342c5bfc8931d255bb861ca4c9c8
abhishek004444 <abhishekjain00444@gmail.com>
2025-10-07 01:31:33 +0530

    Update next.config.js to add allowedDevOrigins and improve formatting

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  
+  // Allow images from these domains
   images: {
     domains: ["example.com"],
+  },
+
+  // If you access dev server from external domains
+  allowedDevOrigins: [
+    "https://is40k4cg8wo0gs80gcgw4kcg.62.72.57.193.sslip.io", // replace with your URL
+  ],
+
+  // Transpile slick-carousel to allow CSS imports from node_modules
+  transpilePackages: ["slick-carousel"],
+
+  // App directory experimental feature
+  experimental: {
+    appDir: true,
   },
 };
 
