d044a71acccf86f8bfcbd4fa70a2c4ff3b22c50e
aartiipcollege <aarti.ggupta2004@gmail.com>
2025-10-27 20:13:48 +0530

    responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,39 +1,39 @@
-import React from "react";
-import Image from "next/image";
-import logo from "@/public/images/logo.png";
+"use client";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { navList } from "@/src/utils/constants";
 import NavLink from "./nav-links";
-import styles from './header.module.scss';
+import styles from "./header.module.scss";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <div className={`${styles.header} container-fluid`}>
-            <div className={`container ${styles.headerWrap}`}>
-                <a href="/" className={styles.logo}>
-                    {/* <Image 
-                        src={logo}
-                        alt="Logo"
-                        className={styles.logoImage}
-                        width={155}
-                        height={50}
-                        priority
-                    /> */}
-                    Sarkari Result
-                </a>
+  return (
+    <div className={`${styles.header} container-fluid`}>
+      <div className={`container ${styles.headerWrap}`}>
+        <a href="/" className={styles.logo}>
+          Sarkari Result
+        </a>
 
-                <nav className={styles.nav}>
-                    <ul>
-                        {navList.map((item, index) => {
-                            return (
-                                <li key={index}>
-                                    <NavLink href={item.path}>{item.name}</NavLink>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </nav>
-            </div>
+        {/* ✅ Toggle Icon for Mobile */}
+        <div
+          className={styles.menuToggle}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <FaTimes /> : <FaBars />}
         </div>
-    );
+
+        {/* ✅ Navigation */}
+        <nav className={`${styles.nav} ${menuOpen ? styles.open : ""}`}>
+          <ul>
+            {navList.map((item, index) => (
+              <li key={index}>
+                <NavLink href={item.path}>{item.name}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
